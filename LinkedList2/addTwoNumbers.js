a34affb0b2e13f7bd7edbc50f4e2b237f43631fd
Nitesh Kumar <82534120+Nitesh8877@users.noreply.github.com>
2023-02-18 22:29:20 +0530

    add second solution

--- LinkedList2/addTwoNumbers.js
+++ LinkedList2/addTwoNumbers.js
@@ -33,7 +33,51 @@ var addTwoNumbers = function(l1, l2) {
     return dummy.next;
 };
 
+// seconde solution
 
+var addTwoNumbers = function(l1, l2) {
+  
+  let head=new ListNode();
+  let curr=head;
+   l1=reverseList(l1);
+    l2=reverseList(l2);
+  let carry=0;
+  let sum=0;
+  while(l1 || l2){
+    let l1val=l1?l1.val:0;
+    let l2val=l2?l2.val:0;
+    sum=l1val+l2val+carry;
+    
+    carry=sum>9?1:0;
+    if(l1){
+      l1.val=sum%10
+      curr.next=l1;
+    }else{
+      l2.val=sum%10;
+      curr.next=l2;
+    }
+    if(l1) l1=l1.next;
+    if(l2) l2=l2.next;
+    curr=curr.next;
+  }
+  if(carry>0){
+    curr.next=new ListNode(1);
+  }
+  let res=reverseList(head.next);
+  return res;
+  
+};
+
+function reverseList(head){
+  let cur=head,prev=null
+  while(cur!=null){
+    let next=cur.next;
+    cur.next=prev;
+    prev=cur;
+    cur=next;
+  }
+  return prev;
+}
 
 
 
